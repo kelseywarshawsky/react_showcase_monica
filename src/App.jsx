40e229ac3645369b1095349a3b6446e4e5b1b41d
MonicaDominguez23 <120822003+MonicaDominguez23@users.noreply.github.com>
2023-08-31 12:01:04 -0500

    Update App.jsx
    made it pretty

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,65 @@
-import { useState } from 'react'
-import './App.css'
-import { Box, Typography, Checkbox, TextField, Button, ListItemButton, ListItemText } from '@mui/material'
-
-
+import { useState } from "react";
+import "./App.css";
+import {
+  Box,
+  Typography,
+  Checkbox,
+  TextField,
+  Button,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 
 function App() {
-  const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+  const label = { inputProps: { "aria-label": "Checkbox demo" } };
   const checkBox = (index) => {
-    const newTodos=[...todos]
-    newTodos[index].checked = !newTodos[index].checked
-    setTodos(newTodos)
-  }
+    const newTodos = [...todos];
+    newTodos[index].checked = !newTodos[index].checked;
+    setTodos(newTodos);
+  };
   const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState("")
- 
- 
+  const [newTodo, setNewTodo] = useState("");
+
   return (
-   <Box>
-    <Typography variant="h1">
-      our to do app
-    </Typography>
-    <div>
-      <Box>
-    <TextField onChange={(e) => setNewTodo(e.target.value)} value={newTodo} id="outlined-basic" label="To Do" variant="outlined" />
+    <Box>
+      <Typography variant="h1">our to do app</Typography>
+      <div>
+        <Box>
+          <TextField
+            onChange={(e) => setNewTodo(e.target.value)}
+            value={newTodo}
+            id="outlined-basic"
+            label="To Do"
+            variant="outlined"
+          />
+        </Box>
+        <Box>
+          {todos.map((todo, index) => (
+            <ListItemButton key={index} component="a" href="#simple-list">
+              <ListItemText primary={todo.name} />
+              <Checkbox
+                onChange={() => checkBox(index)}
+                color="warning"
+                id="1"
+                checked={todo.checked}
+                {...label}
+              />
+            </ListItemButton>
+          ))}
+        </Box>
+
+        <Button
+          onClick={() =>
+            setTodos([...todos, { name: newTodo, checked: false }])
+          }
+          sx={{ marginLeft: "1em" }}
+          variant="contained"
+        >
+          Add To Do
+        </Button>
+      </div>
     </Box>
-<Box>
-{todos.map((todo, index) => (
-    <ListItemButton key={index} component="a" href="#simple-list">
-  <ListItemText primary={todo.name} />
-  <Checkbox onChange={()=> checkBox(index)} color="warning" id= "1" checked={todo.checked} {...label} />
-</ListItemButton>
+  );
+}
 
-))}
-
-</Box>
-
-    <Button onClick={() => setTodos([...todos, {name:newTodo, checked: false}])} sx={{marginLeft: '1em'}} variant="contained" >Add To Do</Button>
-    
-   
-
-
-    </div>
-
-   </Box>
-  )
-  }
-
-export default App
+export default App;
